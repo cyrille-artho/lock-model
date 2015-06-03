924888b5bdd07e91dad3977d9de95e42d6560c06
Cyrille Artho <c.artho@aist.go.jp>
2015-06-03 16:37:46 +0900

    modified:   Environment.java: Refactoring.

--- Environment.java
+++ Environment.java
@@ -3,8 +3,13 @@ import gov.nasa.jpf.vm.Verify;
 class Environment {
   public final static int N_THREADS = 3;
 
+  static Lock createLock() {
+    // factory method to swap out lock impl. in one place
+    return new PrioLock();
+  }
+
   public final static void main(String[] args) {
-    final Lock l = new PrioLock();
+    final Lock l = createLock();
     for (int i = 0; i < N_THREADS; i++) {
       Thread t = new Thread() {
 	public void run() {
