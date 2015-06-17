88c471402d4dd8b22c965d97cd419d4e4124ebab
Cyrille Artho <c.artho@aist.go.jp>
2015-06-17 09:02:04 +0900

    modified:   Diagnostics.java: Comment out code that does not work.

--- Diagnostics.java
+++ Diagnostics.java
@@ -23,17 +23,15 @@ public class Diagnostics extends SearchListenerAdapter {
 //	FieldInfo f1 = tcl.getInstanceField(0);
 //	FieldInfo f2 = tcl.getInstanceField(1);
 	ElementInfo ei = threads[i].getThisElementInfo();
-/*
-	FieldInfo f1 = ei.getFieldInfo("idx1");
-	FieldInfo f2 = ei.getFieldInfo("idx2");
-	if (f1 != null && f2 != null) {
-	  int l1 = ei.getIntField(f1);
-	  int l2 = ei.getIntField(f2);
+//	FieldInfo f1 = ei.getFieldInfo("idx1");
+//	FieldInfo f2 = ei.getFieldInfo("idx2");
+//	if (f1 != null && f2 != null) {
+	  int l1 = ei.getIntField("idx1");
+	  int l2 = ei.getIntField("idx2");
 	  System.out.println("Thread " + threads[i].getId() +
 			     " uses locks " + l1 + 
 			     ", " + l2);
-	}
-*/
+//	}
       }
     }
   }
