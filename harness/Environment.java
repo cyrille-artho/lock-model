2f523433df67313b8a629e06f7609c5b0fd3d7b6
Cyrille Artho <c.artho@aist.go.jp>
2015-06-25 10:31:11 +0900

    modified:   harness/Environment.java: Changed setup to use three threads, with higher priorities for 2nd/3rd thread getting the locks already held by thread 1 (in case that thread runs first).

--- harness/Environment.java
+++ harness/Environment.java
@@ -8,7 +8,7 @@ import rtems.RTEMSThread;
 import gov.nasa.jpf.vm.Verify;
 
 public class Environment {
-  public final static int N_THREADS = 2;
+  public final static int N_THREADS = 3;
   static final Lock[] locks = { createLock(0), createLock(1), createLock(2), createLock(3), createLock(4), createLock(5) };
 
   static Lock createLock(int id) {
@@ -21,25 +21,21 @@ public class Environment {
     /*int li1 = Verify.getInt(0, locks.length - 1);
     int li2 = Verify.getInt(0, locks.length - 1);
     int li3 = Verify.getInt(0, locks.length - 1);*/
-    int li1 = 0;
-    int li2 = 1;
-    int li3 = 2;
-    int li4 = 3;
-    int li5 = 4;
-    int li6 = 5;
-    RTEMSThread t0 = new TestThread(new int[]{li1, li2, li3, li4, li5, li6});
+    int li1 = 1;
+    int li2 = 2;
+    RTEMSThread t0 = new TestThread(new int[]{li1, li2});
     //t0.setPriority(Verify.getInt(1, 3));
     t0.setPriority(3);
     t0.setRealPriority();
     t0.setCurrentPriority();
     System.out.println("Thread 0 has priority " + t0.getPriority() +
-		       " and uses locks " + li1 + ", " + li2 +
-		       ", and " + li3 + ".");
+		       " and uses locks " + li1 + //", " + li2 +
+		       ", and " + li2 + ".");
     t0.start();
     for (int i = 1; i < N_THREADS; i++) {
-      int li = 0;
+      int li = i;
       RTEMSThread t = new TestThread(new int[]{li});
-      t.setPriority(1);
+      t.setPriority(i);
       t.setRealPriority();
       t.setCurrentPriority();
       System.out.println("Thread " + Integer.toString(i + 1) +
